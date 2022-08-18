b9904375a0ec0e229968dbbedb5052659b23afa8
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-08-18 22:05:47 +0530

    Create Set1.cpp

---
+++ STL/Set1.cpp
@@ -0,0 +1,20 @@
+//Print unique strings in lexigraphical order
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	set<string> s;
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		string str;
+		cin>>str;
+		s.insert(str);
+	}
+	for(auto value : s)
+	{
+		cout<<value<<endl;
+	}
+}
