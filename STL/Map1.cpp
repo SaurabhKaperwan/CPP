877403519ca3edf289018881e0cbc8e31d149a4e
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-08-18 10:32:50 +0530

    Create Map1.cpp

---
+++ STL/Map1.cpp
@@ -0,0 +1,23 @@
+//Print unique strings in lexiographical order with their frequency
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	map<string,int> m;
+
+	int n;
+	cin>>n;
+	for (int i = 0; i < n; ++i)
+	{
+		string s;
+		cin>>s;
+		m[s]=m[s]+1;
+	}
+
+	for(auto pr : m)
+	{
+		cout<<pr.first<<" "<<pr.second<<endl;
+	}
+}
