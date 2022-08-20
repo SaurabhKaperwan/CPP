7e464587f036a39287ada1828e6d7187cb8bc0d9
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-08-20 10:38:06 +0530

    Create Map2.cpp

---
+++ STL/Map2.cpp
@@ -0,0 +1,34 @@
+//Sort by marks then name
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	map<int,multiset<string>> v;
+	int n;
+	cin>>n;
+	for (int i = 0; i < n; ++i)
+		{
+			int marks;
+			string name;
+			cin>>name>>marks;
+			marks_map[marks].insert(name);
+
+		}	
+
+		auto cur_it=--marks_map.end();
+		while(true)
+		{
+			auto &students=(*cur_it).second;
+			int marks=(*cur_it).first;
+			for(auto student : students)
+			{
+				cout<<student<<" "<<marks<<endl;
+			}
+			if(cur_it == marks_map.begin())
+			{
+				break;
+			}
+			cur_it--;
+		}
+}
