3c103ed4c9d1693ed504dcb9fd765b2ae2286afe
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-09-02 10:24:27 +0530

    Create Balanced_Parenthesis.cpp

---
+++ STL/Balanced_Parenthesis.cpp
@@ -0,0 +1,50 @@
+//Balanced Parenthesis
+#include<bits/stdc++.h>
+using namespace std;
+
+unordered_map<char,int> symbols={{'[',-1},{'(',-2},{'{',-3},{']',1},{')',2},{'}',3}}; 
+
+string isBalanced(string s)
+{
+	stack<char> st;
+
+	for(char bracket : s)
+	{
+		if(symbols[bracket]<0)
+		{
+			st.push(bracket);
+		}
+		else
+		{
+			if(st.empty())
+				return "No";
+
+			char top=st.top();
+			st.pop();
+
+			if(symbols[top]+symbols[bracket]!=0)
+				return "NO";
+		}
+	}
+
+	if (st.empty())
+		return "YES";
+
+		return "NO";
+
+}
+
+int main()
+{
+	int t;
+	cout<<"Enter test cases:";
+	cin>>t;
+
+	while(t--)
+	{
+		string s;
+		cout<<"Enter string:";
+		cin>>s;
+		cout<<isBalanced(s)<<endl;
+	}
+}
